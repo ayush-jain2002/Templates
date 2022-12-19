d23abde2f069a96ebb31bc06a9017d7671b41d4a
ayush-jain2002 <109455029+ayush-jain2002@users.noreply.github.com>
2022-12-19 16:59:09 +0530

    Add files via upload

---
+++ KMP.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int>prefix_function(string s){
+  int n=s.size();
+  vector<int>p(n,0);
+  for(int i=1;i<n;i++){
+    int j=p[i-1];
+    while(j>0 && s[i]!=s[j]) j=p[j-1];
+    if(s[i]==s[j]) j++;
+    p[i]=j;
+  }
+  return p;
+}
+int main(){ 
+  string s="abccccccabcd";
+  vector<int>p=prefix_function(s);
+  string t="ca";
+  int pos=-1;
+  int i(0),j(0);
+  while(i<s.size()){
+    if(s[i]==t[j]) i++,j++;
+    else{
+      if(j!=0) j=p[j-1];
+      else i++;
+    }
+    if(j==t.size()) pos=i-t.size();
+  }
+  cout<<pos;
+}
