daa8e7f2d6e19b191189acb3445efa1c0f0cbe9e
Ayush Jain <109455029+ayush-jain2002@users.noreply.github.com>
2022-12-26 13:58:18 +0530

    Create Binary Exponentiation.cpp

---
+++ Binary Exponentiation.cpp
@@ -0,0 +1,11 @@
+  int binexp(int a,int b){
+      int result=1;
+      while(b>0){
+          if(b&1){
+              result=(result*1LL*a)%mod;
+          }
+        a=(a*1LL*a)%mod;
+          b>>=1;
+      }
+      return result;
+  }
