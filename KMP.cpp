7597736a8918f145c173bf61cf859841542f82b7
Ayush Jain <109455029+ayush-jain2002@users.noreply.github.com>
2023-04-04 14:14:13 +0530

    Update KMP.cpp

--- KMP.cpp
+++ KMP.cpp
@@ -12,6 +12,20 @@ vector<int>prefix_function(string s){
   }
   return p;
 }
+Optimized algo for z
+// void z(string s) {
+//     int n = s.size();
+//     vector<int> z(n,0);
+//     int x = 0, y = 0;
+//     for (int i = 1; i < n; i++) {
+//         z[i] = max(0,min(z[i-x], y-i+1));
+//         while (i+z[i] < n && s[z[i]] == s[i+z[i]]) {
+//             x = i; y = i + z[i]; z[i]++;
+//         }
+//         if (z[i] + i == n) cout<<i<<' ';
+//     }
+//     cout<<n;
+// }
 int main(){ 
   string s="abccccccabcd";
  
