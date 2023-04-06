b2815e954870e76736c9b56f5fb3d532745fbe02
Ayush Jain <109455029+ayush-jain2002@users.noreply.github.com>
2023-04-06 12:03:02 +0530

    Update Z Algo.cpp

--- Z Algo.cpp
+++ Z Algo.cpp
@@ -26,6 +26,20 @@ void search(string text, string pattern)
 	}
 }
 
+
+// Optimized algo for z
+// void z(string s) {
+//     int n = s.size();
+//     vector<int> z(n,0);
+//     int x = 0, y = 0;
+//     for (int i = 1; i < n; i++) {
+//         z[i] = max(0,min(z[i-x], y-i+1));
+//         while (i+z[i] < n && s[z[i]] == s[i+z[i]]) {
+//             x = i; y = i + z[i]; z[i]++;
+//         }
+//     }
+
+// }
 // Fills Z array for given string str[]
 void getZarr(string str, int Z[])
 {
